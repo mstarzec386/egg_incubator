de902f777b7edbb9454df55c45f817598ffd79b3
Mateusz Starzec <mateusz.starzec@gmail.com>
2021-01-03 15:18:37 +0100

    v0.1.0

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,18 @@
 #define MOTOR 12
 #define HEATER 13
 
+#define COUNTER_MEMORY_ADDR() (EEPROM.length() - 10 * sizeof(unsigned int))
+
+byte cus[] = {
+    B00000,
+    B11111,
+    B10001,
+    B10001,
+    B10001,
+    B11111,
+    B00000,
+    B00000};
+
 // Declare LCD object for software SPI
 // Adafruit_PCD8544(CLK,DIN,D/C,CE,RST);
 Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 8);
@@ -33,13 +45,16 @@ Encoder rotaryEncoder(ROTARY_B, ROTARY_A);
 unsigned long lastDebounceTime = 0;
 int lastButtonState = HIGH;
 
-enum MenuPosition { menuTemp,
+enum MenuPosition { menuDashboard,
+                    menuTemp,
                     menuMinutes,
                     menuDuration,
-                    menuDebugPid,
-                    menuDebugRotate };
+                    menuMaxIterations,
+                    menuManualRotate,
+                    menuSave,
+                    menuSaved };
 
-MenuPosition menuPosition = menuTemp;
+MenuPosition menuPosition = menuDashboard;
 
 struct DHT_OUTPUT {
   float t;
@@ -50,24 +65,22 @@ DHT dht(DHTPIN, DHTTYPE);
 DHT_OUTPUT dhtOutput = {0.0, 0.0};
 
 unsigned long rotateDuration = 2000;
-unsigned int rotateEveryMinutes;
-unsigned long lastRotate;
+unsigned long rotateEveryMinutes = 10;
+unsigned long lastRotate = 0;
+unsigned int maxIterations = 10;
+unsigned int iterateCount = 0;
 bool rotateState = false;
 
 double Setpoint, Input, Output;
 double Kp = 1, Ki = 20, Kd = 30;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
-int WindowSize = 10000;
+int WindowSize = 5000;
 unsigned long windowStartTime;
 
-int MIN_STATE_CHANGE = 1000;
-bool oldChangeState = false;
-unsigned long lastChangeStart = 0;
-
-double handlePID();
+void handlePID();
 void readDHT();
-void handleMenu(double pidOut, long rotaryPosition);
+void handleMenu(long rotaryPosition);
 void handleRotate();
 void readEEPROM();
 void saveEEPROM();
@@ -80,7 +93,7 @@ void setup() {
   pinMode(MOTOR_PIN, OUTPUT);
   pinMode(BULB_PIN, OUTPUT);
   digitalWrite(MOTOR_PIN, HIGH);
-  digitalWrite(BULB_PIN, HIGH);
+  digitalWrite(BULB_PIN, LOW);
 
   pinMode(BACKLIGHT_PIN, OUTPUT);
   turnOnBacklight();
@@ -90,14 +103,14 @@ void setup() {
   lastRotate = millis();
   lastBacklight = millis();
 
-  long oldPosition = (long)rotaryEncoder.read();
+  oldPosition = (long)rotaryEncoder.read();
 
   dht.begin();
 
   windowStartTime = millis();
   Input = (double)dhtOutput.t;
+  myPID.SetOutputLimits(0, WindowSize);
   myPID.SetMode(AUTOMATIC);
-  myPID.SetOutputLimits(0, 100);
 
   //Initialize Display
   display.begin();
@@ -113,7 +126,7 @@ void setup() {
   display.setTextColor(BLACK);
   display.setCursor(0, 0);
   display.println("Egg Incubator");
-  display.println("ver. 0.0.1");
+  display.println("ver. 0.1.0");
   display.display();
   delay(1000);
   display.clearDisplay();
@@ -128,8 +141,8 @@ void loop() {
   }
 
   readDHT();
-  double out = handlePID();
-  handleMenu(out, oldPosition);
+  handlePID();
+  handleMenu(oldPosition);
   handleRotate();
   turnOffBacklight();
 }
@@ -148,8 +161,8 @@ void readEEPROM() {
   }
   idx += sizeof(double);
 
-  //read rotate every minutes
-  unsigned int m;
+  // read motor interval
+  unsigned long m;
   EEPROM.get(idx, m);
   if (m > 0 && m < 60 * 25) {
     rotateEveryMinutes = m;
@@ -157,8 +170,9 @@ void readEEPROM() {
     rotateEveryMinutes = 1;
     EEPROM.put(idx, rotateEveryMinutes);
   }
-  idx += sizeof(unsigned int);
+  idx += sizeof(unsigned long);
 
+  // read motor-on duration
   unsigned int d;
   EEPROM.get(idx, d);
   if (m > 0 && m < 100000) {
@@ -167,17 +181,49 @@ void readEEPROM() {
     rotateDuration = 2000;
     EEPROM.put(idx, rotateDuration);
   }
+  idx += sizeof(unsigned long);
+
+  // read max motor iterations
+  unsigned int maxIter;
+  EEPROM.get(idx, maxIter);
+  if (maxIter >= 0 && maxIter < 1000) {
+    maxIterations = maxIter;
+  } else {
+    maxIterations = 10;
+    EEPROM.put(idx, maxIterations);
+  }
   idx += sizeof(unsigned int);
+
+  // get current timers
+  // get iteration count at the end of eeprom
+  unsigned int iterCount;
+  EEPROM.get(COUNTER_MEMORY_ADDR(), iterCount);
+  if (iterCount >= 0 && iterCount < 1000) {
+    iterateCount = iterCount;
+  } else {
+    iterateCount = iterCount;
+    EEPROM.put(COUNTER_MEMORY_ADDR(), iterateCount);
+  }
 }
 
 void saveEEPROM() {
   int idx = 0;
+  // set temperature setpoint
   EEPROM.put(idx, Setpoint);
   idx += sizeof(double);
+  // set motor interval
   EEPROM.put(idx, rotateEveryMinutes);
-  idx += sizeof(unsigned int);
+  idx += sizeof(unsigned long);
+  // set motor-on duration
   EEPROM.put(idx, rotateDuration);
+  idx += sizeof(unsigned long);
+  // set max iterations
+  EEPROM.put(idx, maxIterations);
   idx += sizeof(unsigned int);
+
+  // at the end of eeprom
+  // set iteration count
+  EEPROM.put(COUNTER_MEMORY_ADDR(), iterateCount);
 }
 
 void turnOffBacklight() {
@@ -193,47 +239,39 @@ void turnOnBacklight() {
   backlightOn = true;
 }
 
-double handlePID() {
+void handlePID() {
   Input = (double)dhtOutput.t;
   myPID.Compute();
 
-  if (millis() - windowStartTime > WindowSize) {
+  if (millis() - windowStartTime > (unsigned long)WindowSize) {
     windowStartTime += WindowSize;
   }
 
-  double out = Output * 100;
-
-  if (out < 500) {
-    out = 0;
-  }
-
-  if (out < millis() - windowStartTime) {
-    if (oldChangeState && millis() - lastChangeStart > MIN_STATE_CHANGE) {
-      oldChangeState = false;
-      lastChangeStart = millis();
-      digitalWrite(BULB_PIN, HIGH);
-    }
+  if (Output < millis() - windowStartTime) {
+    digitalWrite(BULB_PIN, LOW);
   } else {
-    if (!oldChangeState && millis() - lastChangeStart > MIN_STATE_CHANGE) {
-      oldChangeState = true;
-      lastChangeStart = millis();
-      digitalWrite(BULB_PIN, LOW);
-    }
+    digitalWrite(BULB_PIN, HIGH);
   }
-
-  return out;
 }
 
 void handleRotate() {
+  if (iterateCount >= maxIterations)
+    return;
+
   if (millis() - lastRotate > rotateEveryMinutes * 60 * 1000) {
     rotateState = true;
     digitalWrite(MOTOR_PIN, LOW);
+    digitalWrite(MOTOR_PIN, LOW);
   }
 
-  if (rotateState && millis() - lastRotate > (unsigned long)(rotateEveryMinutes * 60 * 1000) + rotateDuration) {
+  if (rotateState && millis() - lastRotate > (rotateEveryMinutes * 60 * 1000) + rotateDuration) {
     digitalWrite(MOTOR_PIN, HIGH);
     lastRotate = millis();
     rotateState = false;
+    iterateCount += 1;
+    // saveEEPROM();
+    // writes optimization - writes counter only
+    EEPROM.put(COUNTER_MEMORY_ADDR(), iterateCount);
   }
 }
 
@@ -249,8 +287,8 @@ void readDHT() {
   }
 }
 
-void handleMenu(double pidOut, long rotaryPosition) {
-  //handle menu key
+void handleMenu(long rotaryPosition) {
+  // handle menu key
   int button = digitalRead(ROTARY_BUTTON);
 
   if (button == LOW && millis() - lastDebounceTime > 500) {
@@ -259,6 +297,9 @@ void handleMenu(double pidOut, long rotaryPosition) {
     turnOnBacklight();
 
     switch (menuPosition) {
+    case menuDashboard:
+      menuPosition = menuTemp;
+      break;
     case menuTemp:
       menuPosition = menuMinutes;
       break;
@@ -266,57 +307,109 @@ void handleMenu(double pidOut, long rotaryPosition) {
       menuPosition = menuDuration;
       break;
     case menuDuration:
-      menuPosition = menuDebugPid;
+      menuPosition = menuMaxIterations;
       break;
-    case menuDebugPid:
-      menuPosition = menuDebugRotate;
+    case menuMaxIterations:
+      menuPosition = menuManualRotate;
       break;
-    case menuDebugRotate:
-      menuPosition = menuTemp;
+    case menuManualRotate:
+      menuPosition = menuSave;
+      break;
+    case menuSave:
+      menuPosition = menuDashboard;
+      break;
+    case menuSaved:
+      menuPosition = menuDashboard;
       break;
     }
   }
 
   display.clearDisplay();
-  display.setTextSize(2);
-  display.setCursor(0, 0);
-  display.print(dhtOutput.h);
-  display.println("%");
-  display.print(dhtOutput.t);
-  display.println("C");
 
   switch (menuPosition) {
-  case menuTemp:
+  case menuDashboard: {
+    display.setTextSize(2);
+    display.setCursor(0, 0);
+    display.print(dhtOutput.h);
+    display.println("%");
+    display.print(dhtOutput.t);
+    display.println("C");
+    display.setTextSize(1);
+    display.print(Setpoint);
+    display.print("C ");
+    display.print(iterateCount);
+    display.print("/");
+    display.println(maxIterations);
+
+    if (iterateCount >= maxIterations) {
+      display.print("KONIEC");
+    } else {
+      unsigned long nextRotate = (rotateEveryMinutes * 60 * 1000 - (millis() - lastRotate)) / 1000;
+
+      if (nextRotate < 1000000) {
+        unsigned long hh = nextRotate / (60 * 60);
+        unsigned long mm = (nextRotate - (hh * 60 * 60)) / 60;
+        unsigned long ss = nextRotate - (hh * 60 * 60 + mm * 60);
+        display.print(hh);
+        display.print("h");
+        display.print(mm);
+        display.print("m");
+        display.print(ss);
+        display.print("s");
+      } else {
+        display.print("Obrot");
+      }
+    }
+
+    break;
+  }
+  case menuTemp: {
     if (diffPosition != 0) {
       double r = roundf((double)diffPosition / 4) / 10.0;
       double d = roundf(r * 10.0) / 10.0;
       Setpoint += d;
       diffPosition = 0;
-      saveEEPROM();
     }
+
+    display.setTextSize(1);
+    display.println(" Temperatura");
+    display.println("  ustawiona");
+    display.println("");
     display.setTextSize(2);
-    display.print("T:");
-    display.println(Setpoint);
+    display.print(Setpoint);
+    display.println("C");
+
     break;
-  case menuMinutes:
+  }
+  case menuMinutes: {
     if (diffPosition != 0) {
-      unsigned int d = round((double)diffPosition / 4);
+      int d = round((double)diffPosition / 4);
       rotateEveryMinutes += d;
-      if (rotateEveryMinutes < 1) {
+
+      if (rotateEveryMinutes < 1 || rotateEveryMinutes > 9999) {
         rotateEveryMinutes = 1;
       }
 
       lastRotate = millis();
       diffPosition = 0;
-      saveEEPROM();
     }
+
+    unsigned int h = rotateEveryMinutes / 60;
+    unsigned int m = rotateEveryMinutes - (h * 60);
+    display.setTextSize(1);
+    display.println("  Obrot co");
     display.setTextSize(2);
-    display.print("M:");
-    display.print(rotateEveryMinutes);
+    display.println("");
+    display.print(h);
+    display.print("h");
+    display.print(m);
+    display.print("m");
+
     break;
-  case menuDuration:
+  }
+  case menuDuration: {
     if (diffPosition != 0) {
-      unsigned long d = round((double)diffPosition / 4);
+      int d = round((double)diffPosition / 4);
       rotateDuration += d * 100;
       if (rotateDuration < 100) {
         rotateDuration = 100;
@@ -324,36 +417,78 @@ void handleMenu(double pidOut, long rotaryPosition) {
 
       lastRotate = millis();
       diffPosition = 0;
-      saveEEPROM();
     }
+
+    display.setTextSize(1);
+    display.println(" Obrot przez");
     display.setTextSize(2);
-    display.print("D:");
+    display.println("");
     display.print(roundf(((float)rotateDuration / 1000.0) * 10.0) / 10.0);
+    display.print("s");
     break;
-  case menuDebugPid:
+  }
+  case menuMaxIterations: {
+    if (diffPosition != 0) {
+      int d = round((double)diffPosition / 4);
+      maxIterations += d;
+      if (maxIterations < 1) {
+        maxIterations = 1;
+      }
+
+      iterateCount = 0;
+      diffPosition = 0;
+    }
     display.setTextSize(1);
-    display.print(millis() - windowStartTime);
-    display.print("; ");
-    display.println(round(pidOut));
-    display.print(rotaryPosition);
-    display.print(";");
-    display.print(diffPosition);
-    display.print(";");
-    display.print(digitalRead(ROTARY_BUTTON));
-    /* code */
-    break;
-  case menuDebugRotate:
+    display.println("Max obrotow");
+    display.setTextSize(2);
+    display.print(" ");
+    display.println(maxIterations);
     display.setTextSize(1);
-    display.print(millis() - lastRotate);
-    display.print(";");
-    display.println(rotateDuration);
-    display.print(rotateEveryMinutes * 60 * 1000);
-    display.print(";");
-    display.print((unsigned long)(rotateEveryMinutes * 60 * 1000) + rotateDuration);
-    display.print(";");
-    display.print(rotateState);
-    /* code */
+    display.println("Zrobionych");
+    display.setTextSize(2);
+    display.print(" ");
+    display.print(iterateCount);
+
     break;
+  }
+  case menuManualRotate: {
+    if (diffPosition > 0) {
+      digitalWrite(MOTOR_PIN, LOW);
+    } else {
+      digitalWrite(MOTOR_PIN, HIGH);
+    }
+
+    display.setTextSize(1);
+    display.println("Manualny Obrot");
+    display.setTextSize(2);
+    display.println("");
+    display.print("   ");
+    display.println(diffPosition > 0 ? "X" : "-");
+    break;
+  }
+  case menuSave: {
+    display.setTextSize(1);
+    display.println("");
+    display.setTextSize(2);
+    display.println("Zapisz");
+    display.setTextSize(1);
+    display.println("obroc w prawo");
+
+    if (diffPosition > 5) {
+      saveEEPROM();
+      menuPosition = menuSaved;
+    }
+    break;
+  }
+  case menuSaved: {
+    display.setTextSize(1);
+    display.println("");
+    display.println(" ------------");
+    display.println("| Ustawienia |");
+    display.println("|  zapisane  |");
+    display.println(" ------------");
+    break;
+  }
   }
 
   display.display();
